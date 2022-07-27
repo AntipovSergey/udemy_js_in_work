1b2d3910615c343aa043d0c359d489a44dc8f5c0
Sergey Antipov <antip99999@mail.ru>
2022-07-27 10:46:50 +0300

    Create repo and create tabs behaviour

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorAll('.tabcontent'),
+            tabsParent = document.querySelector('.tabheader__items');
+
+    const hideContent = () => {
+        tabsContent.forEach((tab) => {
+            tab.classList.add('hide');
+            tab.classList.remove('show', 'fade');
+        })
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tabheader__item_active');
+        })
+    }
+
+    const showContent = (i = 0) => {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideContent();
+    showContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((tab, i) => {
+                if (tab === target) {
+                    hideContent();
+                    showContent(i);
+                }
+            })
+        }
+    })
+})
